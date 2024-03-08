447c70f200a81212cd4d84872d721e70290b3699
Herman Contreras <60008328+hcontr4@users.noreply.github.com>
2024-03-08 06:06:31 +0000

    api call

--- app.rb
+++ app.rb
@@ -1,6 +1,33 @@
 require "sinatra"
 require "sinatra/reloader"
 
+require "http"
+require "json"
+
+CATEGORY_CODE = {
+  "film" => 11,
+  "science" => 17,
+  "history" => 23,
+  "sports" => 21,
+  "geography" => 22
+}
+
 get("/") do
   erb(:landing)
+end
+
+get("/:category") do
+  @category = params[:category]
+
+  api_url = "https://opentdb.com/api.php?amount=1&category=#{CATEGORY_CODE[@category]}&difficulty=medium&type=multiple"
+  raw_response = HTTP.get(api_url)
+  parsed_response = JSON.parse(raw_response)
+
+  question_block = parsed_response["results"].first
+
+  @question = question_block["question"]
+  @correct_answer = question_block["correct_answer"]
+  @incorrect_answers = question_block["incorrect_answers"]
+  
+  erb(:question)
 end
