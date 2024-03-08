0b8539b74293f0afaf1fe3595f3f88cbdc72340a
Herman Contreras <60008328+hcontr4@users.noreply.github.com>
2024-03-08 07:51:12 +0000

    Handle api call with no content in response

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ CATEGORY_CODE = {
   "science" => 17,
   "history" => 23,
   "sports" => 21,
-  "geography" => 22
+  "geography" => 22,
 }
 
 get("/") do
@@ -22,13 +22,18 @@ get("/:category") do
   api_url = "https://opentdb.com/api.php?amount=1&category=#{CATEGORY_CODE[@category]}&difficulty=medium&type=multiple"
   raw_response = HTTP.get(api_url)
   parsed_response = JSON.parse(raw_response)
+  # api call did not return question, wait a bit and ask again
+  unless parsed_response["response_code"] == 0
+    sleep(0.5)
+    redirect ("/#{@category}")
+  end
 
   question_block = parsed_response["results"].first
 
   @question = question_block["question"]
   @correct_answer = question_block["correct_answer"]
   @options = (question_block["incorrect_answers"] << @correct_answer).shuffle()
-  
+
   erb(:question)
 end
 
